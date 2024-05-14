eefbca75bbdb9d8ea17c29ad25935a5ae265a2a5
Llamalectric <lotsnlotsofllamas@gmail.com>
2024-05-14 16:20:07 -0700

    add Person and Numbers classes

---
+++ Person.java
@@ -0,0 +1,35 @@
+public class Person {
+	private String firstName;
+	private String lastName;
+
+	public Person(String fname) {
+		firstName = fname;
+		lastName = "";
+	}
+
+	public Person(String fname, String lname) {
+		firstName = fname;
+		lastName = lname;
+	}
+
+	// Getter and Setter for firstName
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String name) {
+		firstName = name;
+		return;
+	}
+
+	// Getter and Setter for lastName
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String name) {
+		lastName = name;
+		return;
+	}
+
+}
