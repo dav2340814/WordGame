1ce7146b3b88c9f7ff9b820f9b04a921f263f25b
Llamalectric <lotsnlotsofllamas@gmail.com>
2024-06-14 15:33:44 -0700

    Revert "fix compilation error"
    This reverts commit 11be3cb679f7160983f9197bca8617dd99dce63e.

--- GUI.java
+++ GUI.java
@@ -31,14 +31,11 @@ public class GUI {
 			public void actionPerformed(ActionEvent e) {
 				GamePlay.currentPlayers
 						.add(new Players(JOptionPane.showInputDialog("New Player Name: ")));
-				if (!GamePlay.currentPlayers.isEmpty()) {
-					String oldPlayers = playersLabel.getText();
-					playersLabel.setText(
-							oldPlayers + GamePlay.currentPlayers.getLast().getFirstName()
-									+ GamePlay.currentPlayers.getLast()
-											.getLastName()
-									+ ", ");
-				}
+				String oldPlayers = playersLabel.getText();
+				playersLabel.setText(
+						oldPlayers + GamePlay.currentPlayers.getLast().getFirstName()
+								+ GamePlay.currentPlayers.getLast().getLastName()
+								+ ", ");
 				frame.pack();
 
 			}
